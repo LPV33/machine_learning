7804c40a72e9ab21c8196a63d737d04a3ca24669
plaptev <plaptev@mera.ru>
2019-01-15 17:15:07 +0300

    K-means method. Сompleted

--- kmeans.py
+++ kmeans.py
@@ -15,12 +15,14 @@
 
 #print(__doc__)
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin
 from sklearn.datasets import load_sample_image
 from sklearn.utils import shuffle
 from time import time
+from sklearn.metrics import mean_squared_error
 
 from skimage.io import imread
 from skimage import img_as_float
@@ -41,35 +43,7 @@ def recreate_image(codebook, labels, w, h):
 def ClusterIndicesComp(clustNum, labels_array): #list comprehension
     return np.array([i for i, x in enumerate(labels_array) if x == clustNum])
 
-def recreate_image_median_colors(km, orig_image, labels, w, h):
-    """Recreate the (compressed) image from the code book & labels"""
-    d = km.cluster_centers_.shape[1]
-    codebook = []
-    n_clusters = km.n_clusters
-    for n_cl in range(n_clusters):
-        #n_cl_mean = np.mean([ClusterIndicesComp(n_cl, km.labels_)])
-        n_cluster_labels = ClusterIndicesComp(n_cl, km.labels_)
-        X = orig_image[n_cluster_labels]
-        codebook.append(np.median(X, axis=0))
-    """
-    This is an example of how to calculate means values
-        r_mean = np.mean(X[:,0])
-        g_mean = np.mean(X[:,1])
-        b_mean = np.mean(X[:,2])
-        ##mean = np.amin(X, axis=0)
-        mean =[r_mean, g_mean, b_mean]
-        codebook.append(mean)
-    """
-
-
-    image = np.zeros((w, h, d))
-    label_idx = 0
-    for i in range(w):
-        for j in range(h):
-            image[i][j] = codebook[labels[label_idx]]
-            label_idx += 1
-    return image
-
+# Return a codebook with median colors
 def calc_median_colors(n_clusters, orig_image, labels):
     """Recreate the (compressed) image from the code book & labels"""
     d = 3 #RGB
@@ -91,6 +65,14 @@ def calc_median_colors(n_clusters, orig_image, labels):
     """
     return np.array(codebook)
 
+#Return Peak Signal Noise Ratio (PSNR)
+def calc_psnr (orig_image, clust_image):
+    Max = 1.0
+    #mse = mean_squared_error (orig_image[:,0], clust_image[:,0])
+    mse = ((orig_image - clust_image) ** 2).mean(axis=0)
+    psnr = 20.0 * math.log10(Max) - 10.0 * math.log10(mse.max())
+    return psnr
+
 origin_image = imread('parrots.jpg')
 converted_img = img_as_float(origin_image)
 
@@ -106,27 +88,40 @@ image_array = np.reshape(converted_img, (w * h, d))
 
 
 
-for n_clusters in range(7):
+for n_clusters in range(20):
     kmeans = KMeans(n_clusters=n_clusters+1, random_state=241, init='k-means++').fit(image_array)
 
     # Get labels for all points
 
     labels = kmeans.predict(image_array)
 
-    #Draw picture with mean colors
-    plt.figure(2*(n_clusters+1))
-    plt.clf()
-    plt.axis('off')
-    plt.title('Clusters')
-    plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
+    image_with_means_colors = np.reshape(recreate_image(kmeans.cluster_centers_, labels, w, h), (w * h, d))
+    psnr_means = calc_psnr(image_array, image_with_means_colors)
 
-    #Draw picture with median colors
-    plt.figure(2*(n_clusters+1)+1)
-    plt.clf()
-    plt.axis('off')
-    plt.title('Clusters with mean color')
-    codebook_median_colors = calc_median_colors(n_clusters+1, image_array, labels)
-    plt.imshow(recreate_image(codebook_median_colors, labels, w, h))
+    codebook_median_colors = calc_median_colors(n_clusters + 1, image_array, labels)
+    image_with_median_colors = np.reshape(recreate_image(codebook_median_colors, labels, w, h), (w * h, d))
+    psnr_medians = calc_psnr(image_array, image_with_median_colors)
+
+    print ("Clusters: \%d. Mean colors PSNR \%f. Median colors PSNR \%f\n", n_clusters, psnr_means, psnr_medians)
+
+    #Draw picture with mean colors
+    if (False):  # You decide to display the pictures or not
+        plt.figure(2*(n_clusters+1))
+        plt.clf()
+        plt.axis('off')
+        plt.title('Clusters')
+        plt.imshow(image_with_means_colors)
+
+
+    # Draw picture with median colors
+    if (False): #You decide to display the pictures or not
+        plt.figure(2*(n_clusters+1)+1)
+        plt.clf()
+        plt.axis('off')
+        plt.title('Clusters with mean color')
+        plt.imshow(image_with_median_colors)
+
+
 
 #codebook_random = shuffle(image_array, random_state=0)[:n_colors]
 #print("Predicting color indices on the full image (random)")
@@ -137,17 +132,11 @@ for n_clusters in range(7):
 #print("done in %0.3fs." % (time() - t0))
 
 
-
-# Display all results, alongside original image
+"""
+# Display original image
 plt.figure(0)
 plt.clf()
 plt.axis('off')
 plt.title('Original image (96,615 colors)')
 plt.imshow(converted_img)
-"""
-plt.figure(2)
-plt.clf()
-plt.axis('off')
-plt.title('Quantized image (64 colors, K-Means)')
-plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
 """
